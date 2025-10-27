337dcd4c44842a1d0201a1f906037a69fa35a0fb
tarekfouda <tarekfouda0@gmail.com>
2025-10-27 12:51:58 +0300

    set default lang

--- lib/features/home/domain/requester/languages_requester.dart
+++ lib/features/home/domain/requester/languages_requester.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_tdd/core/constants/app_constants.dart';
 import 'package:flutter_tdd/core/helpers/di.dart';
+import 'package:flutter_tdd/core/helpers/global_state.dart';
 import 'package:flutter_tdd/core/requester/requester.dart';
 import 'package:flutter_tdd/features/home/data/model/lang_model/lang_model.dart';
 import 'package:flutter_tdd/features/home/domain/repositories/home_repositories.dart';
@@ -8,6 +10,12 @@ class LanguagesRequester extends Requester<List<LangModel>> {
   Future<void> request({bool fromRemote = true}) async {
     var result = await getIt<HomeRepositories>().getLanguages(fromRemote);
     result.when(isSuccess: (data) {
+      String? currentLang = GlobalState.instance.get(ApplicationConstants.langKey);
+      for (var lang in data??<LangModel>[]) {
+        if (lang.code == (currentLang ?? ApplicationConstants.langEN)) {
+          lang.isDefault = true;
+        }
+      }
       successState(data ?? []);
     }, isError: (error) {
       failedState(error, () {});
